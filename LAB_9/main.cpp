1bb1735fce7486d1cdee99483db6ee8a4b5435ff
myshyak5 <stameska40@mail.ru>
2025-11-06 21:47:22 +0200

    Add files via upload

---
+++ LAB_9/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+void heapify(int* arr, int n, int i){
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+    if (largest != i) {
+        std::swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+void heapSort(int* arr, int n) {
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+    for (int i = n - 1; i > 0; i--) {
+        std::swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+const int n = 5;
+int arr[n] = {2, 3, 1, 6, 4};
+
+int main(){
+    heapSort(arr, n);
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
